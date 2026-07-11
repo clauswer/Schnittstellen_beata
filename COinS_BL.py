c86034e94d26d960ae0642e6d00cd60aa5204649
Beatka-star <beata.lakeberg@gmx.de>
2026-07-11 19:39:10 +0200

    Hausaufgaben_3

--- COinS_BL.py
+++ COinS_BL.py
@@ -21,46 +21,49 @@ from urllib.parse import parse_qs
 
 DEBUG=True
 
-#Seite KOBV aufrufen und die Anfrage bauen
 
-url = "https://portal.kobv.de/simpleSearch.do" #Die KOBV Webseite, die einfache Suche aufruft
+# Liest COinS-Metadaten von einer KOBV-Titelseite aus. Der Link zur Titelseite wird von nutzer angegeben
+def kobv_abfrage (kataloglink):
 
-def kobv_abfrage (suchbegriff):
-    params = {
-        "index": "internal", # Suche in internen Index
-        "plv": "2", #Layout Version von Seite'
-        "sortCrit": "score", #Sortierung nach Relevanz
-        "sortOrder": "desc", #Reihenfolge absteigend
-        "hitsPerPage": "10",  # 10 Ergebnise pro Seite
-        "query": suchbegriff  # hier wird die Variable eingesetzt, um die einfache Suche durchzuführen
-    }
-    response = requests.get(url, params=params)
-    print(response.url)
+#Prüfen, ob die Verbindung funktioniert:
+    try:
+        response = requests.get(url=kataloglink)
+    except Exception as e:
+         print(f"\nFehler in der Verbindung:\n{e}")
+         sys.exit(1)
+    #Wenn die Fehlermeldung kommt, kann es an folgenden liegen: Server ist nicht erreichbar; kein Internet;URL ist falsch geschrieben
 
-    """Prüfen, ob die Verbindung funktioniert"""
-try:
-    response = requests.get(url)
-    #print(response.status_code) #muss nicht sein, beim Test gut zu sehen
-except Exception as e:
-    print(f"\nFehler in der Verbindung:\n{e}")
-    sys.exit(1)
+    # Statuscode prüfen, wenn Fehlermeldung kommt, kann es an folgenden liegen: Seite nicht gefunden (404); Zugriff verweigert (403); Serverfehler (500)
+    if response.status_code != 200:
+        print("Fehler! Statuscode:", response.status_code)
+        sys.exit(1)
 
-if DEBUG:
-    print(response.url)  # Debug-Ausgabe der tatsächlichen URL
+    if DEBUG:
+        print("URL:", response.url)
+        print("Statuscode:", response.status_code)
 
     # Sicherstellen, dass die Antwort als UTF-8 dekodiert wird
-response.encoding = response.apparent_encoding
+    response.encoding = response.apparent_encoding
+
+def parse_results (html_inhalt):
+    #liest COinS-Metadaten aus der HTML aus.
+    soup = BeautifulSoup(html_inhalt, "html.parser") #BeautifulSoup macht HTML durchsuchbar
+    print(html_inhalt[:2000])  #Prüft ob KOBV überhaupt sinnvolle HTML-Daten zurückschickt. Nur zum Testen
+
+    alle_coins = soup.find_all("span", class_="Z3988") #Findet alle Elemente, die Name "span" und Klasse "Z3988" haben
+    print(len(alle_coins)) #Gibt alle gefundene COinS aus
+
+    #for coins in alle_coins:
+
+    """print("Autor:     ", autor.text if autor is not None else "-")
+    print("Titel:     ", titel.text if titel is not None else "nicht vorhanden")
+    print("ISSN:      ", isbn.text if isbn is not None else "nicht vorhanden")
+    print("Seiten:", herausgeber.text if herausgeber is not None else "-") """
+
+
 # Menü
 if __name__ == "__main__":
-    suchbegriff = input("Bitte geben Sie ein Suchbegriff an: ")
-    kobv_abfrage(suchbegriff)
-soup = BeautifulSoup(response.content, "html.parser")
-print(response.text[:2000])  #Prüft ob KOBV überhaupt sinnvolle HTML-Daten zurückschickt.
-alle_coins = soup.find_all("span", class_="Z3988")
-print(len(alle_coins))
-
-# und hier ist was falsch, weil er findent die COinS nicht!!!!!
-#print(alle_coins[0]["title"])
-
+    kataloglink = input("Bitte geben Sie ein Link zum Exemplar an: ")
+    kobv_abfrage(kataloglink)
 
 
